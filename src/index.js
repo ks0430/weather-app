4f0066fc304f9a077cf41cebb985c6b0ba79de7e
Evelyn <ks0430@163.com>
2019-01-19 15:37:45 +1000

    Add logger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
-
-
+// Portect user infomations
+const helmet = require('helmet');
 // Modules
 const express = require('express');
 // process in production
 const PORT = process.env.PORT || 3000; 
-// Configs
+// Configs - before use routes
 require('dotenv').config();
-
 const routes = require('./routes');
+// Log extraction
+const morgan = require('morgan');
+// 
+const logger = require('../utils/logger');
+
+
+
 
 // Create app instance 
 const app = express();
 
 
+// Portect user infomations
+app.use(helmet());
+// Log extraction
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+} else {
+    app.use(morgan('common'));
+}
 // Import routes
 app.use(routes);
 
+
+
 // Start listening
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+app.listen(PORT,() => logger.info(`Listening on port ${PORT}`));
